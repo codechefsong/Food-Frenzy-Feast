f896696af7ece53420409f553886307bc1c9e6de
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-01-21 14:33:54 -0500

    add take food feature

--- packages/hardhat/contracts/FoodFrenzyFeast.sol
+++ packages/hardhat/contracts/FoodFrenzyFeast.sol
@@ -8,6 +8,9 @@ contract FoodFrenzyFeast {
   Counters.Counter public numberOfMatches;
 
   Match[] public matchList;
+  mapping(uint256 => uint256[]) public games;
+  mapping(uint256 => address[]) public players;
+  mapping(address => uint256[]) public playerBag;
 
   struct Match {
     uint256 id;
@@ -30,5 +33,28 @@ contract FoodFrenzyFeast {
 
   function joinMatch(uint256 _matchId) external {
     matchList[_matchId].numberOfPlayers += 1;
+    players[_matchId].push(msg.sender);
+  }
+
+  function startGame(uint256 _matchId) external {
+    for(uint i = 0; i < 8; i++){
+      uint256 _randomNumber = uint(keccak256(abi.encode(block.timestamp, msg.sender, i))) % 8;
+      games[_matchId].push(_randomNumber);
+    }
+  }
+
+  function takeFood(uint256 _matchId) external {
+    uint256 position = getPlayerPosition(_matchId, msg.sender);
+    playerBag[msg.sender].push(games[_matchId][position]);
+    uint256 _randomNumber = uint(keccak256(abi.encode(block.timestamp, msg.sender))) % 8;
+    games[_matchId][position] = _randomNumber;
+  }
+
+  function getPlayerPosition(uint256 _matchId, address _player) internal view returns (uint256) {
+    for (uint i = 0; i < players[_matchId].length; i++) {
+      if (players[_matchId][i] == _player) return i;
+    }
+
+    return 0;
   }
 }
